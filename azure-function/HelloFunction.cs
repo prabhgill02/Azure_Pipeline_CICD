4ce416c6253ae94551ba2bc58148a52350b6784a
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-01 17:47:20 -0400

    Added Azure function

---
+++ azure-function/HelloFunction.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+public static class HelloFunction
+{
+    [FunctionName("HelloFunction")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Hello, world!");
+    }
+}
